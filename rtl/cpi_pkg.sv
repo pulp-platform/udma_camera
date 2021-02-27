28edd67f2911e0b920471b01f37ca6024bed6d63
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-02-27 18:13:30 +0100

    add cpi pkg

---
+++ rtl/cpi_pkg.sv
@@ -0,0 +1,16 @@
+package cpi_pkg;
+	 // cpi structure
+	typedef struct packed{
+		logic pclk_i;
+		logic hsync_i;
+		logic vsync_i;
+		logic data0_i;
+		logic data1_i;
+		logic data2_i;
+		logic data3_i;
+		logic data4_i;
+		logic data5_i;
+		logic data6_i;
+		logic data7_i;
+	}pad_to_cpi_t;
+endpackage
