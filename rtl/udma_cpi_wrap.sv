203c8579f848cd92f8613e553f0f1bd421421dfb
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-07-30 16:45:27 +0200

    add licence header

--- rtl/udma_cpi_wrap.sv
+++ rtl/udma_cpi_wrap.sv
@@ -1,4 +1,21 @@
- module udma_cpi_wrap
+/* 
+ * Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
+ *
+ * Copyright (C) 2018-2020 ETH Zurich, University of Bologna
+ * Copyright and related rights are licensed under the Solderpad Hardware
+ * License, Version 0.51 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *                http://solderpad.org/licenses/SHL-0.51. 
+ *
+ * Unless required by applicable law
+ * or agreed to in writing, software, hardware and materials distributed under
+ * this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+module udma_cpi_wrap
     import udma_pkg::udma_evt_t;
     import cpi_pkg::pad_to_cpi_t;
 (
@@ -87,7 +104,6 @@ assign events_o[3]   = 1'b0;
 // assigning unused signals
 assign rx_ch[0].stream      = '0;
 assign rx_ch[0].stream_id   = '0;
-//assign rx_ch[0].destination = '0;
 
 endmodule : udma_cpi_wrap
 
